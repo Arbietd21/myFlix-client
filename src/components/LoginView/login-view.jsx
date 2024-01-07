650c37a088fc8fb34fcdd75d08d3d37fcb0eefd9
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2024-01-07 00:10:07 -0600

    created login-view.jsx

---
+++ src/components/LoginView/login-view.jsx
@@ -0,0 +1,11 @@
+export const LoginView = () => {
+    return (
+        <form>
+            <label>Username:</label>
+            <input type="text" />
+            <label>Password:</label>
+            <input type="password" />
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
