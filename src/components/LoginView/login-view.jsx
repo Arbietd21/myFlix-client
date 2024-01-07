5c7907c7624da7753c38e7a68a794616f2d88a53
Arbietd21 <arbiealemany14@gmail.com>
2024-01-07 16:48:13 -0600

    update login-view.jsx

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -33,11 +33,13 @@ export const LoginView = ({ onLoggedIn }) => {
             <input type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            required
             />
             <label>Password:</label>
             <input type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
             />
             <button type="submit">Submit</button>
         </form>
