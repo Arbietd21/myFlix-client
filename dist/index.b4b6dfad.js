0fcae694dcb915cd85b2810f2af91c7bbeda66f7
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2024-01-08 19:11:12 -0600

    updated login-view.jsx

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -28335,27 +28335,25 @@ const LoginView = ({ onLoggedIn })=>{
     const [username, setUsername] = (0, _react.useState)("");
     const [password, setPassword] = (0, _react.useState)("");
     const handleSubmit = (event)=>{
-        const handleSubmit = (event)=>{
-            //this prevents the default behaviour of the form which is to reload the entire page
-            event.preventDefault();
-            const data = {
-                Usernamme: username,
-                Password: password
-            };
-            fetch("https://movie-flix-api-ca627b5a7961.herokuapp.com/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then((response)=>response.json()).then((data)=>{
-                console.log("Login response: ", data);
-                if (data.user) onLoggedIn(data.user, data.token);
-                else alert("No such user");
-            }).catch((e)=>{
-                alert("Something went wrong");
-            });
+        //this prevents the default behaviour of the form which is to reload the entire page
+        event.preventDefault();
+        const data = {
+            Username: username,
+            Password: password
         };
+        fetch("https://movie-flix-api-ca627b5a7961.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then((response)=>response.json()).then((data)=>{
+            console.log("Login response: ", data);
+            if (data.user) onLoggedIn(data.user, data.token);
+            else alert("No such user");
+        }).catch((e)=>{
+            alert("Something went wrong");
+        });
     };
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("form", {
         onSubmit: handleSubmit,
--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -4,36 +4,36 @@ import { useState } from 'react';
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+
     const handleSubmit = (event) => {
-        const handleSubmit = (event) => {
-            //this prevents the default behaviour of the form which is to reload the entire page
-            event.preventDefault();
+        //this prevents the default behaviour of the form which is to reload the entire page
+        event.preventDefault();
 
-            const data = {
-                Usernamme: username,
-                Password: password
-            };
-
-            fetch("https://movie-flix-api-ca627b5a7961.herokuapp.com/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then((response) => response.json())
-                .then((data) => {
-                    console.log("Login response: ", data);
-                    if (data.user) {
-                        onLoggedIn(data.user, data.token);
-                    } else {
-                        alert("No such user");
-                    }
-                })
-                .catch((e) => {
-                    alert("Something went wrong")
-                });
+        const data = {
+            Username: username,
+            Password: password
         };
+
+        fetch("https://movie-flix-api-ca627b5a7961.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong")
+            });
     };
+
 
     return (
         <form onSubmit={handleSubmit}>
