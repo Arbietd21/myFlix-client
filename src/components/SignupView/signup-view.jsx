520c21ef147750ca52809a1e47f758b190ae7937
Arbietd21 <145313814+Arbietd21@users.noreply.github.com>
2024-01-12 00:17:47 -0600

    updated signup-view.jsx

--- src/components/SignupView/signup-view.jsx
+++ src/components/SignupView/signup-view.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 export const SignupView = () => {
     const [username, setUsername] = useState("");
@@ -32,51 +33,53 @@ export const SignupView = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-
-            <label>
-                Username:
-                <input
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required
                     minLength="3"
                 />
-            </label>
+            </Form.Group>
+            <Form.Group>
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+            </Form.Group>
+            <Form.Group>
+                <label>
+                    Email:
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </label>
+            </Form.Group>
+            <Form.Group>
+                <label>
+                    Birthday:
+                    <input
+                        type="date"
+                        value={birthday}
+                        onChange={(e) => setBirthday(e.target.value)}
+                        required
+                    />
+                </label>
+            </Form.Group>
 
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
+            <Button variant="primary" type="submit">Submit</Button>
 
-            <label>
-                Email:
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-            </label>
-
-            <label>
-                Birthday:
-                <input
-                    type="date"
-                    value={birthday}
-                    onChange={(e) => setBirthday(e.target.value)}
-                    required
-                />
-            </label>
-
-            <button type="submit">Submit</button>
-
-        </form>
+        </Form>
     )
 };
